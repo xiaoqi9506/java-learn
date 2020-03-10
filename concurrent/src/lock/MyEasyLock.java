16b771357374cb24abcbe7f03dc2b7cf7ce44eca
zxq0620 <xiaoqi9506@163.com>
2020-03-10 13:35:26 +0800

    简单锁实现

---
+++ concurrent/src/lock/MyEasyLock.java
@@ -0,0 +1,51 @@
+package lock;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * 简单锁，继承自AbstractQueuedSynchronizer
+ */
+public class MyEasyLock extends AbstractQueuedSynchronizer {
+
+    /**
+     * 加锁方法
+     */
+    public void lock() {
+        //注意这里调用的是抽象类里的acquire方法，而不是重写的tryAcquire方法
+        acquire(1);
+    }
+
+    /**
+     * 需要重写抽象类的这个方法，因为抽象类里没有具体实现
+     * 这个方法主要是加锁逻辑，可以在这里实现一些复杂的加锁逻辑
+     * 我这里只是实现简单的独占锁，所以逻辑比较简单
+     */
+    @Override
+    protected boolean tryAcquire(int arg) {
+        if (getState() == 0 && compareAndSetState(0, 1)) {
+            setExclusiveOwnerThread(Thread.currentThread());
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 解锁方法
+     */
+    public void unlock() {
+        //注意这里调用的是抽象类里的release方法，而不是重写的tryRelease方法
+        release(1);
+    }
+
+    /**
+     * 需要重写抽象类的这个方法，因为抽象类里没有具体实现
+     * 这个方法主要是解锁逻辑，需要对加上的锁一个个解开
+     */
+    @Override
+    protected boolean tryRelease(int arg) {
+        if (getState() == 0) throw new IllegalMonitorStateException();
+        setExclusiveOwnerThread(null);
+        setState(0);
+        return true;
+    }
+}
---
+++ concurrent/test/TestMyLock.java
@@ -0,0 +1,46 @@
+import lock.MyEasyLock;
+
+/**
+ * 测试自定义锁
+ */
+public class TestMyLock {
+
+    /**
+     * 多线程共享资源，倒数至0，每个数字只能使用一次
+     */
+    private static int num = 100;
+
+    /**
+     * synchronized锁对象
+     */
+    private static final Object obj = new Object();
+
+    /**
+     * 自定义锁
+     */
+    private static final MyEasyLock lock = new MyEasyLock();
+
+    public static void main(String[] args) {
+
+        //10个线程同时消耗共享资源，很明显会有线程安全问题，会重复打印部分数字
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                lock.lock();
+                while (num > 0) {
+                    //解决方式1 synchronized同步代码块
+                    //synchronized (obj) {
+                    //    System.out.println("当前num值为：" + --num);
+                    //}
+                    try {
+                        System.out.println("当前num值为：" + --num);
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        System.out.println("程序异常");
+                        lock.unlock();
+                    }
+                }
+                lock.unlock();
+            }).start();
+        }
+    }
+}
